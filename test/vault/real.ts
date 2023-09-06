c94faabc2bd33bd1226fb72202762e03e9e2d56e
M7 <m7@M7s-MacBook-Pro-2.local>
2023-09-06 19:44:12 +0530

    Adding the real liquidation test case

--- test/vault/real.ts
+++ test/vault/real.ts
@@ -1,6 +1,6 @@
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
-import { ethers, web3 } from "hardhat";
+import { ethers, tasks, web3 } from "hardhat";
 import { toN, deployGLPVaultKeeper } from "../utils";
 import { time } from "@nomicfoundation/hardhat-network-helpers";
 
@@ -150,7 +150,7 @@ describe("Reality checks", function(){
 
             // Continued testing where u2 deposits
 
-            // deposit 10000 tokens at 5x leverage
+            // deposit 100 tokens at 5x leverage
             await glp.connect(u2).approve(vaultManager.address, toN(9900));
             await glp.mint(u2.address, toN(2000));
             await vaultManager.connect(u2).deposit(toN(100), toN(5), 0, 0);
@@ -208,5 +208,190 @@ describe("Reality checks", function(){
             console.log('hf: ', await glpVault.hf(u2.address)/1e18);
 
             expect(Math.round(await glp.balanceOf(stgy.address)/1e14)).to.equal(71); // Everything is withdrawn nothing pending in strategy expect dust balance
+    });
+
+
+    it("10% APY - Target PPS : ~1.07 | Liquidation | Stakes | Excess", async function(){
+        const {owner, u1, u2, u3, usdc, glp, wETH, phMock, pool, oGlp,
+             vaultKeeper, vaultManager, glpVault, fees, stgy, glpMockRouter, glpMockManager, oliveManager} = await loadFixture(deployGLPVaultKeeper);
+
+            // deposit 10000 tokens at 5x leverage
+            await glp.approve(vaultManager.address, toN(9900));
+            await glp.mint(u1.address, toN(9900));
+            await vaultManager.connect(u1).deposit(toN(1900), toN(5), 0, 0);
+            
+            // 10% APY 
+            await phMock.setPriceOf(wETH.address, toN(1));
+            await glpMockRouter.setFeesToClaim(toN(1000)); // 10% yield
+            let pps = (await glpVault.getPPS()/1e16);
+            time.increase(365 * 24 * 3600); // After one year
+            await vaultKeeper.connect(u1).harvest();
+            console.log(await glpVault.getPPS()/1e16);
+
+            
+            // Close user position
+            console.log('-- user1 --');
+            console.log('Postion: ', await glpVault.getPosition(u1.address)/1e18);
+            console.log('Debt', await glpVault.getDebt(u1.address)/1e18);
+
+            // deposit 100 Tokens, his position would be liquidated
+            await glp.connect(u2).approve(vaultManager.address, toN(10000));
+            await glp.mint(u2.address, toN(100));
+            await vaultManager.connect(u2).deposit(toN(100), toN(5), 0, 0);
+
+            expect(Math.round(await oGlp.balanceOf(u2.address)/1e16)).to.equal(46729);
+            expect(Math.round(await glpVault.getPosition(u2.address)/1e18)).to.equal(500);
+
+            time.increase(365 * 24 * 3600); // After one year
+            await glpMockManager.setPriceOfGLP(toN(0.9, 30));
+            console.log('\n\n-- user1 --');
+            console.log('Postion: ', await glpVault.getPosition(u2.address)/1e18);
+            console.log('Debt', await glpVault.getDebt(u2.address)/1e18);
+            console.log('Hf', await glpVault.hf(u2.address)/1e18);
+
+            await usdc.mint(owner.address, toN(500, 6));
+            await vaultKeeper.setLiquidator(owner.address, true);
+            await usdc.approve(pool.address, toN(415, 6));
+            await vaultKeeper.liquidation(u2.address, toN(415, 6), true);
+
+            console.log('oGLP : ', await oGlp.balanceOf(u2.address)/1e18);
+            console.log('Postion: ', await glpVault.getPosition(u2.address)/1e18);
+            console.log('Debt', await glpVault.getDebt(u2.address)/1e18);
+            console.log('Hf', await glpVault.hf(u2.address)/1e18);
+
+            console.log('\n\n-- liquidator --');
+            console.log('oGLP : ', await oGlp.balanceOf(owner.address)/1e18);
+            console.log('Postion: ', await glpVault.getPosition(owner.address)/1e18);
+            console.log('Debt', await glpVault.getDebt(owner.address)/1e18);
+            console.log('Hf', await glpVault.hf(owner.address)/1e18);
+
+            console.log('\n\n-- treasury --');
+            let ta = await fees.getTreasury();
+            console.log('oGLP : ', await oGlp.balanceOf(ta)/1e18);
+            console.log('Postion: ', await glpVault.getPosition(ta)/1e18);
+            console.log('Debt', await glpVault.getDebt(ta)/1e18);
+            console.log('Hf', await glpVault.hf(ta)/1e18);
+
+            // -- user1 --
+            // Postion:  500
+            // Debt 457.9797766666667
+            // Hf 0.982576137477628
+            // want allowance: 415000000
+            // toPayInAsset: 412181800
+            // oGLP :  17.870311685201887
+            // Postion:  19.121233333333333
+            // Debt 0.000001111111111111
+            // Hf 15488199.000001548
+
+
+            // -- liquidator --
+            // oGLP :  445.13922721432135
+            // Postion:  476.2989688888889
+            // Debt 0
+            // Hf 1.157920892373162e+59
+
+
+            // -- treasury --
+            // oGLP :  4.280184877060782
+            // Postion:  4.579797777777777
+            // Debt 0
+            // Hf 1.157920892373162e+59
+            
+            expect(Math.round(await oGlp.balanceOf(u2.address)/1e18)).to.equal(18);
+            expect(Math.round(await oGlp.balanceOf(owner.address)/1e18)).to.equal(445);
+            expect(Math.round(await oGlp.balanceOf(ta)/1e18)).to.equal(4);
+    });
+
+
+    it("10% APY - Target PPS : ~1.07 | Liquidation | UnStakes | Excess", async function(){
+        const {owner, u1, u2, usdc, glp, wETH, phMock, pool, oGlp,
+             vaultKeeper, vaultManager, glpVault, fees, glpMockRouter, glpMockManager} = await loadFixture(deployGLPVaultKeeper);
+
+            // deposit 10000 tokens at 5x leverage
+            await glp.approve(vaultManager.address, toN(9900));
+            await glp.mint(u1.address, toN(9900));
+            await vaultManager.connect(u1).deposit(toN(1900), toN(5), 0, 0);
+            
+            // 10% APY 
+            await phMock.setPriceOf(wETH.address, toN(1));
+            await glpMockRouter.setFeesToClaim(toN(1000)); // 10% yield
+            let pps = (await glpVault.getPPS()/1e16);
+            time.increase(365 * 24 * 3600); // After one year
+            await vaultKeeper.connect(u1).harvest();
+            console.log(await glpVault.getPPS()/1e16);
+
+            
+            // Close user position
+            console.log('-- user1 --');
+            console.log('Postion: ', await glpVault.getPosition(u1.address)/1e18);
+            console.log('Debt', await glpVault.getDebt(u1.address)/1e18);
+
+            // deposit 100 Tokens, his position would be liquidated
+            await glp.connect(u2).approve(vaultManager.address, toN(10000));
+            await glp.mint(u2.address, toN(100));
+            await vaultManager.connect(u2).deposit(toN(100), toN(5), 0, 0);
+
+            expect(Math.round(await oGlp.balanceOf(u2.address)/1e16)).to.equal(46729);
+            expect(Math.round(await glpVault.getPosition(u2.address)/1e18)).to.equal(500);
+
+            time.increase(365 * 24 * 3600); // After one year
+            await glpMockManager.setPriceOfGLP(toN(0.9, 30));
+            console.log('\n\n-- user1 --');
+            console.log('Postion: ', await glpVault.getPosition(u2.address)/1e18);
+            console.log('Debt', await glpVault.getDebt(u2.address)/1e18);
+            console.log('Hf', await glpVault.hf(u2.address)/1e18);
+
+            await usdc.mint(owner.address, toN(500, 6));
+            await vaultKeeper.setLiquidator(owner.address, true);
+            await usdc.approve(pool.address, toN(415, 6));
+            await vaultKeeper.liquidation(u2.address, toN(415, 6), false);
+
+            console.log('oGLP : ', await oGlp.balanceOf(u2.address)/1e18);
+            console.log('Postion: ', await glpVault.getPosition(u2.address)/1e18);
+            console.log('Debt', await glpVault.getDebt(u2.address)/1e18);
+            console.log('Hf', await glpVault.hf(u2.address)/1e18);
+
+            console.log('\n\n-- liquidator --');
+            console.log('oGLP : ', await oGlp.balanceOf(owner.address)/1e18);
+            console.log('Postion: ', await glpVault.getPosition(owner.address)/1e18);
+            console.log('Debt', await glpVault.getDebt(owner.address)/1e18);
+            console.log('Hf', await glpVault.hf(owner.address)/1e18);
+
+            console.log('\n\n-- treasury --');
+            let ta = await fees.getTreasury();
+            console.log('oGLP : ', await oGlp.balanceOf(ta)/1e18);
+            console.log('Postion: ', await glpVault.getPosition(ta)/1e18);
+            console.log('Debt', await glpVault.getDebt(ta)/1e18);
+            console.log('Hf', await glpVault.hf(ta)/1e18);
+
+            // -- user1 --
+            // Postion:  500
+            // Debt 457.9797766666667
+            // Hf 0.982576137477628
+            // want allowance: 415000000
+            // toPayInAsset: 412181800
+            // oGLP :  17.870311685201887
+            // Postion:  19.121233333333333
+            // Debt 0.000001111111111111
+            // Hf 15488199.000001548
+
+
+            // -- liquidator --
+            // oGLP :  445.13922721432135
+            // Postion:  476.2989688888889
+            // Debt 0
+            // Hf 1.157920892373162e+59
+
+
+            // -- treasury --
+            // oGLP :  4.280184877060782
+            // Postion:  4.579797777777777
+            // Debt 0
+            // Hf 1.157920892373162e+59
+            
+            expect(Math.round(await oGlp.balanceOf(u2.address)/1e18)).to.equal(18);
+            expect(Math.round(await oGlp.balanceOf(owner.address)/1e18)).to.equal(0);
+            expect(Math.round(await glp.balanceOf(owner.address)/1e18)).to.equal(476);
+            expect(Math.round(await oGlp.balanceOf(ta)/1e18)).to.equal(4);
     });
 });
